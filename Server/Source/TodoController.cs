08c6756ef1d72fb4701db3c488a2f634ede34062
Jeremy Collette <jeremy.collette93@gmail.com>
2019-06-03 22:36:21 -0700

    Server no longer depends on client models as they are generated from the server

--- Server/Source/TodoController.cs
+++ Server/Source/TodoController.cs
@@ -3,60 +3,46 @@
     using System;
     using System.Collections.Generic;
 
-    using AutoMapper;
     using Microsoft.AspNetCore.Mvc;
 
     using todoweb.Server.Core;
-    using ClientTodo = todoweb.Client.Todo;
-    using CoreTodo = todoweb.Server.Models.Todo;
-
+    using todoweb.Server.Models;
 
     [Route("", Name = "default")]
     public class TodoController : Controller
     {
         private TodoManager todoManager_ = TodoManager.Instance;
-        private IMapper mapper_;
-
-        public TodoController()
-        {
-            var config = new MapperConfiguration(cfg =>
-            {
-                cfg.CreateMap<ClientTodo, CoreTodo>();
-                cfg.CreateMap<CoreTodo, ClientTodo>();
-            });
-            mapper_ = config.CreateMapper();
-        }
 
         // GET todo/
         [HttpGet]
-        public IEnumerable<ClientTodo> Get()
+        public IEnumerable<Todo> Get()
         {
-            return mapper_.Map<IEnumerable<CoreTodo>, IEnumerable<ClientTodo>>(todoManager_.GetAll());
+            return todoManager_.GetAll();
         }
 
         // GET todo/5
         [HttpGet("{id}")]
-        public ClientTodo Get(Guid id)
+        public Todo Get(Guid id)
         {
-            return mapper_.Map<CoreTodo, ClientTodo>(todoManager_.Get(id));
+            return todoManager_.Get(id);
         }
 
         // POST todo/
         [HttpPost]
-        public ClientTodo Create([FromBody]ClientTodo todo)
+        public Todo Create([FromBody]Todo todo)
         {
             todo.Id = Guid.NewGuid();
-            todoManager_.Add(mapper_.Map<ClientTodo, CoreTodo>(todo));
+            todoManager_.Add(todo);
             return todo;
         }
 
         // PUT todo/5
         [HttpPut("{id}")]
-        public ClientTodo CreateOrUpdate(Guid id, [FromBody]ClientTodo todo)
+        public Todo CreateOrUpdate(Guid id, [FromBody]Todo todo)
         {
             todo.Id = id;
             todoManager_.Delete(id);
-            todoManager_.Add(mapper_.Map<ClientTodo, CoreTodo>(todo));
+            todoManager_.Add(todo);
             return todo;
         }
 
