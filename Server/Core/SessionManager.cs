e389ff6b9ce2cafd1a75e0647cb3480a094c2254
Jeremy Collette <jeremy.collette93@gmail.com>
2019-08-02 16:04:46 -0700

    Fixed integration test

--- Server/Core/SessionManager.cs
+++ Server/Core/SessionManager.cs
@@ -8,50 +8,49 @@
 
     public class SessionManager : ISessionManager
     {
-        Dictionary<Guid, Guid> sessionToUserMap_;
-        Dictionary<Guid, Guid> userToSessionMap_;
+        // TODO(@jecollet): remove statics
+        static Dictionary<Guid, Guid> sessionToUserMap_ = new Dictionary<Guid, Guid>();
+        static Dictionary<Guid, Guid> userToSessionMap_ = new Dictionary<Guid, Guid>();
         IResourceManager<User> userManager_;
 
         public SessionManager(DatabaseContext<User> databaseContext)
         {
-            this.sessionToUserMap_ = new Dictionary<Guid, Guid>();
-            this.userToSessionMap_ = new Dictionary<Guid, Guid>();
             this.userManager_ = new DatabaseResourceManager<User>(databaseContext);
         }
 
         public Guid CreateOrUpdateSession(User user)
         {
-            if (this.userToSessionMap_.ContainsKey(user.Id))
+            if (SessionManager.userToSessionMap_.ContainsKey(user.Id))
             {
-                return this.userToSessionMap_[user.Id];
+                return SessionManager.userToSessionMap_[user.Id];
             }
 
             var sessionId = Guid.NewGuid();
-            this.sessionToUserMap_[sessionId] = user.Id;
-            this.userToSessionMap_[user.Id] = sessionId;
+            SessionManager.sessionToUserMap_[sessionId] = user.Id;
+            SessionManager.userToSessionMap_[user.Id] = sessionId;
             return sessionId;
         }
 
         public bool DeleteSession(Guid sessionId)
         {
-            if (!this.sessionToUserMap_.ContainsKey(sessionId))
+            if (!SessionManager.sessionToUserMap_.ContainsKey(sessionId))
             {
                 return false;
             }
 
-            var userId = this.sessionToUserMap_[sessionId];
-            this.sessionToUserMap_.Remove(sessionId);
-            this.userToSessionMap_.Remove(userId);
+            var userId = SessionManager.sessionToUserMap_[sessionId];
+            SessionManager.sessionToUserMap_.Remove(sessionId);
+            SessionManager.userToSessionMap_.Remove(userId);
             return true;
         }
 
         public User GetUserFromSession(Guid sessionId)
         {
-            if (!this.sessionToUserMap_.ContainsKey(sessionId))
+            if (!SessionManager.sessionToUserMap_.ContainsKey(sessionId))
             {
                 return null;
             }
-            return this.userManager_.Get(this.sessionToUserMap_[sessionId]);
+            return this.userManager_.Get(SessionManager.sessionToUserMap_[sessionId]);
         }
     }
 }
