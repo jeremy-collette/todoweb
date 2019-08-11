8baef0284d91598345c63eee1fa29d65f5f65d39
Jeremy Collette <jeremy.collette93@gmail.com>
2019-08-11 13:55:04 -0700

    Added 'Complete' button

--- Client/Source/Client.cs
+++ Client/Source/Client.cs
@@ -4,7 +4,6 @@
 // </auto-generated>
 //----------------------
 
-using System;
 using todoweb.Client.Models;
 
 #pragma warning disable 108 // Disable "CS0108 '{derivedDto}.ToJson()' hides inherited member '{dtoBase}.ToJson()'. Use the new keyword if hiding was intended."
@@ -18,6 +17,13 @@ namespace todoweb.Client
     [System.CodeDom.Compiler.GeneratedCode("NSwag", "13.0.1.0 (NJsonSchema v10.0.19.0 (Newtonsoft.Json v11.0.0.0))")]
     public partial interface ITodoClient
     {
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<Todo> CompleteAsync(System.Guid id);
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<Todo> CompleteAsync(System.Guid id, System.Threading.CancellationToken cancellationToken);
+    
         /// <exception cref="SwaggerException">A server side error occurred.</exception>
         System.Threading.Tasks.Task<Todo> CreateAsync(Todo resource);
     
@@ -78,6 +84,76 @@ namespace todoweb.Client
         partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, string url);
         partial void PrepareRequest(System.Net.Http.HttpClient client, System.Net.Http.HttpRequestMessage request, System.Text.StringBuilder urlBuilder);
         partial void ProcessResponse(System.Net.Http.HttpClient client, System.Net.Http.HttpResponseMessage response);
+    
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        public System.Threading.Tasks.Task<Todo> CompleteAsync(System.Guid id)
+        {
+            return CompleteAsync(id, System.Threading.CancellationToken.None);
+        }
+    
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <exception cref="SwaggerException">A server side error occurred.</exception>
+        public async System.Threading.Tasks.Task<Todo> CompleteAsync(System.Guid id, System.Threading.CancellationToken cancellationToken)
+        {
+            if (id == null)
+                throw new System.ArgumentNullException("id");
+    
+            var urlBuilder_ = new System.Text.StringBuilder();
+            urlBuilder_.Append(BaseUrl != null ? BaseUrl.TrimEnd('/') : "").Append("/api/todo/complete/{id}");
+            urlBuilder_.Replace("{id}", System.Uri.EscapeDataString(ConvertToString(id, System.Globalization.CultureInfo.InvariantCulture)));
+    
+            var client_ = _httpClient;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    request_.Content = new System.Net.Http.StringContent(string.Empty, System.Text.Encoding.UTF8, "application/json");
+                    request_.Method = new System.Net.Http.HttpMethod("POST");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("application/json"));
+    
+                    PrepareRequest(client_, request_, urlBuilder_);
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+                    PrepareRequest(client_, request_, url_);
+    
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    try
+                    {
+                        var headers_ = System.Linq.Enumerable.ToDictionary(response_.Headers, h_ => h_.Key, h_ => h_.Value);
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+    
+                        ProcessResponse(client_, response_);
+    
+                        var status_ = ((int)response_.StatusCode).ToString();
+                        if (status_ == "200") 
+                        {
+                            var objectResponse_ = await ReadJsonObjectResponseAsync<Todo>(response_, headers_).ConfigureAwait(false);
+                            return objectResponse_.Object;
+                        }
+                        else
+                        if (status_ != "200" && status_ != "204")
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false); 
+                            throw new SwaggerException("The HTTP status code of the response was not expected (" + (int)response_.StatusCode + ").", (int)response_.StatusCode, responseData_, headers_, null);
+                        }
+            
+                        return default(Todo);
+                    }
+                    finally
+                    {
+                        if (response_ != null)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+            }
+        }
     
         /// <exception cref="SwaggerException">A server side error occurred.</exception>
         public System.Threading.Tasks.Task<Todo> CreateAsync(Todo resource)
@@ -587,7 +663,7 @@ namespace todoweb.Client
                 return settings;
             });
         }
-
+    
         protected Newtonsoft.Json.JsonSerializerSettings JsonSerializerSettings { get { return _settings.Value; } }
     
         partial void UpdateJsonSerializerSettings(Newtonsoft.Json.JsonSerializerSettings settings);
--- Server/Source/TodoController.cs
+++ Server/Source/TodoController.cs
@@ -1,5 +1,7 @@
 ﻿namespace todoweb.Server
 {
+    using System;
+
     using Microsoft.AspNetCore.Mvc;
 
     using todoweb.Server.Contract;
@@ -12,9 +14,42 @@
     public class TodoController
         : ResourceController<Client.Todo, Server.Todo>
     {
-        public TodoController(IResourceManager<Server.Todo> userManager, IHttpSessionManager httpSessionManager, IAuthorizationPolicy<Server.Todo> authorizationPolicy)
-            : base(userManager, httpSessionManager, authorizationPolicy)
+        private IResourceManager<Server.Todo> todoManager_;
+        private IHttpSessionManager httpSessionManager_;
+        private IAuthorizationPolicy<Server.Todo> authorizationPolicy_;
+
+        public TodoController(IResourceManager<Server.Todo> todoManager, IHttpSessionManager httpSessionManager, IAuthorizationPolicy<Server.Todo> authorizationPolicy)
+            : base(todoManager, httpSessionManager, authorizationPolicy)
         {
+            this.todoManager_ = todoManager;
+            this.httpSessionManager_ = httpSessionManager;
+            this.authorizationPolicy_ = authorizationPolicy;
+        }
+
+        [Route("complete/{id}")]
+        [HttpPost]
+        public ActionResult Complete(Guid id)
+        {
+            var user = this.httpSessionManager_.GetUserFromRequest(Request);
+            if (user == null)
+            {
+                return Unauthorized();
+            }
+
+            var todo = this.todoManager_.Get(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            if (!authorizationPolicy_.CanWrite(user, todo))
+            {
+                return Unauthorized();
+            }
+
+            todo.Done = true;
+            this.todoManager_.AddOrUpdate(todo);
+            return Ok();
         }
     }
 }
